e6fa170f44f1e16435524553971d7970b9783527
i.altoubah <i.altoubah@haynespro.com>
2020-09-07 15:13:25 +0200

    Write Main Class to test the DB connection

---
+++ src/main/java/it/fiat/manual/Application.java
@@ -0,0 +1,35 @@
+package it.fiat.manual;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import it.fiat.manual.jse.jpa.service.FiatMakeRepo;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+
+
+	@Autowired
+	private FiatMakeRepo fiatMakeRepo;
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("Did it wire by repo? {}", fiatMakeRepo);
+		fiatMakeRepo.getAll();
+
+	}
+
+}
