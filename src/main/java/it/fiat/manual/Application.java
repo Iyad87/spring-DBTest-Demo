c40cc2ff297d22c13f6ccb7f37736aff5c986d88
i.altoubah <i.altoubah@haynespro.com>
2020-09-07 16:17:08 +0200

    Modified Main Class

--- src/main/java/it/fiat/manual/Application.java
+++ src/main/java/it/fiat/manual/Application.java
@@ -1,6 +1,9 @@
 package it.fiat.manual;
 
 
+import java.sql.Connection;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.fiat.manual.jse.jpa.DBConnection;
+import it.fiat.manual.jse.jpa.FiatDao;
 import it.fiat.manual.jse.jpa.FiatDocMake;
 import it.fiat.manual.jse.jpa.service.FiatMakeRepo;
 
@@ -21,7 +26,10 @@ public class Application implements CommandLineRunner {
 	@Autowired
 	private FiatMakeRepo fiatMakeRepo;
 
+	@Autowired
+	DBConnection dbConnection;
 
+	Connection connection;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -31,9 +39,13 @@ public class Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		LOG.info("Did it wire by repo? {}", fiatMakeRepo);
 		FiatDocMake fiatDocMake = new FiatDocMake(1,"090909090","FiatDescription");
+		dbConnection.fiatDataSource();
+		List<FiatDocMake> fiatDocMakes =  fiatMakeRepo.getAll();
+//		String sql = "SELECT * FROM fiatDocModel";
+//		connection.prepareStatement(sql);
+//		FiatDao fiatDao = new FiatDao(connection);
+//		fiatDao.insertFiatMode(fiatDocMake);
 
-		fiatMakeRepo.getAll();
-
+		System.out.println(fiatDocMakes);
 	}
-
 }
