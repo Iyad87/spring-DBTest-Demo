8d481f00387393ace20ba730b75423701fe5e798
i.altoubah <i.altoubah@haynespro.com>
2020-09-10 15:36:27 +0200

    change xml file dir

--- src/main/java/it/fiat/manual/config/DbConfig.java
+++ src/main/java/it/fiat/manual/config/DbConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("it.fiat.manual.jse")
+@MapperScan("it.fiat.manual.jse.repository")
 
 public class DbConfig {
 
